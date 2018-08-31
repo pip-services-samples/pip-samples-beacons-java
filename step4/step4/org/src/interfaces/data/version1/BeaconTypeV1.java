c67c9122931b21446b28eaa3573ef78f8bef7315
EgorNuzhnykh <nuzhnykhev@gmail.com>
2018-08-31 10:18:06 +0300

    Fixed code restructuring by steps

--- step4/step4/org/src/interfaces/data/version1/BeaconTypeV1.java
+++
@@ -1,8 +0,0 @@
-package step4.org.src.interfaces.data.version1;
-
-public enum BeaconTypeV1 {
-	Unknown, 
-    AltBeacon,
-    iBeacon,
-    EddyStoneUdi
-}
--- step8/step8/org/src/interfaces/data/version1/BeaconTypeV1.java
+++
@@ -1,8 +0,0 @@
-package step8.org.src.interfaces.data.version1;
-
-public enum BeaconTypeV1 {
-	Unknown, 
-    AltBeacon,
-    iBeacon,
-    EddyStoneUdi
-}
