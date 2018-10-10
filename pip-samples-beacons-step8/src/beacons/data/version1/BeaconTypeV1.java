170bcd9ded6979a00b2d8cbcb581860f306b1327
EgorNuzhnykh <nuzhnykhev@gmail.com>
2018-10-10 15:21:08 +0300

    Fixed sample by steps

--- pip-samples-beacons-step8/src/beacons/data/version1/BeaconTypeV1.java
+++
@@ -1,8 +0,0 @@
-package beacons.data.version1;
-
-public class BeaconTypeV1 {
-	public static String Unknown = "unknown"; 
-    public static String AltBeacon = "altbeacon";
-    public static String iBeacon = "ibeacons";
-    public static String EddyStoneUdi = "eddistoneudi";
-}
