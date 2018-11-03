05dd0f8817d37236b09e4888385c11d7c0e8a23d
Mark Makarychev <markmak77@gmail.com>
2018-11-03 13:59:01 +0300

    Updated the release script.

--- step8/release.ps1
+++ step8/release.ps1
@@ -4,15 +4,21 @@ Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-$version = (Get-Content -Path package.json | ConvertFrom-Json).version
+$version = ([xml](Get-Content -Path "pom.xml")).project.version
 
 if ($component.version -ne $version) {
-    throw "Versions in component.json and package.json do not match"
+    throw "Versions in component.json and pom.xml do not match"
 }
 
-# Automatically login to server
-if ($env:NPM_USER -ne $null -and $env:NPM_PASS -ne $null) {
-    npm-cli-login
-}
-
-npm publish
+# Make sure that:
+# 1. GPG has been installed, key was generated and uploaded to a key server;
+# 2. Maven has been installed (use v3.3.9 instead of v3.5.4 - otherwise upload will be VERY slow);
+# 3. Maven's global settings file (~/.m2/settings.xml) contains all necessary credentials and your key
+# 4. ~/.gnupg contains 2 files, each of which contain the lines in double quotation marks:
+#   4a. ~/.gnupg/gpg-agent.conf
+#   "allow-loopback-pinentry"
+#   4b. ~/.gnupg/gpg.conf
+#   "pinentry-mode loopback"
+# 
+# See "Development.md" in the "doc" folder for detailed instructions.
+mvn clean deploy
